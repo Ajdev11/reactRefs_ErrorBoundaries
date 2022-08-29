c9364b2ea6c76717112ab3365c03c97dd7853e13
Ajdev11 <mosesogun11@gmail.com>
2022-08-29 14:42:16 -0700

    also added thearrow the function to the handler in the button directly

--- src/Components/stateClass.jsx
+++ src/Components/stateClass.jsx
@@ -15,20 +15,23 @@ class State extends Component {
      //pass an handler or event listener
      //alternatively the binding method can be called in the handleclass also
      //lets an example of that below
-     handleMessage = () =>{
+     handleMessage(){
         //call setState
         this.setState({
             message : "thanks for seeing me"
         })
      }
+     //also the arrow function can be passed into the button directly 
+     //example below
     render() { 
         return (
             <>
             <h4>{this.state.message}</h4>
-            <button onClick={this.handleMessage}>Subscribe</button>
+            <button onClick={()=> this.handleMessage()}>Subscribe</button>
             </>
         );
     }
 }
+//however the binding method in the constructor and the class should be the best approach going forward
  
 export default State;
