f3cd2ec85bf6435d405896dc529dc1e7380879fe
Ajdev11 <mosesogun11@gmail.com>
2022-08-29 17:15:41 -0700

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
+import "./index.css"
 import FunctionalApp from './Components/type of components';
 import ClassComponents from './Components/classcomp';
 import Hello from './Components/props';
 import ClassProps from './Components/propsclass';
 import State from './Components/stateClass';
 import PropsMethod from './Components/methodProps';
+import ConditionalRendering from './Components/conditions';
+
+
+
 class App extends Component {
     state = {  } 
     render() { 
         return (
-            <>
-            <FunctionalApp />
+           /* <>
+            <FunctionalApp  />
             <ClassComponents />
             <Hello name = "Moses" otherNames = "Olaseni" />
             <ClassProps aka = "Engineer" alias ="Baddest" />
             <State />
-            <PropsMethod />
+            <PropsMethod  />
+            </>*/
+            <>
+            <ConditionalRendering />
             </>
         );
     }
---
+++ src/Components/conditions.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class ConditionalRendering extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoggedIn: true,
+    };
+  }
+  render(){
+   //example of the if/else statement
+    /*if (this.state.isLoggedIn) {
+      return <div>Welcome MOses</div>;
+    } else {
+      return <div>WElcome Guest</div>;
+    }
+    //example of the elemnet variavble
+    /*let message;
+        if(this.state.isLoggedIn){
+            message = <div>Welcome Moses</div>
+        }else{
+            message = <div>Goodbye Moses</div>
+        }
+        return <div>{message}</div>*/
+    //the best approach for conditional rendering is the tenary method;
+ return (
+    this.state.isLoggedIn ? (<idv>Welcome AJdEV</idv>) : (<div>Goodbye Ajdev</div>)
+ )
+  }
+}
+
+export default ConditionalRendering;
